3c44c701dfd8c54412557cb0be7235cf43f67635
Darek <sevisilex@gmail.com>
2024-12-20 14:19:07 +0100

    refactor: Abstract localStorage operations

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,13 +1,17 @@
 import { Language } from '../i18n'
 
-export const setDarkModeStorage = (isDark: boolean): void => localStorage.setItem('theme', isDark ? 'dark' : 'light')
+const setStorageItem = <T>(key: string, value: T): void => localStorage.setItem(key, JSON.stringify(value))
+const getStorageItem = <T>(key: string): T | null => {
+  const item = localStorage.getItem(key)
+  // return item ? (JSON.parse(item) as T) : null
+  return item ? item as T : null
+}
 
-export const isDarkModeStorage = (): boolean => localStorage.getItem('theme') === 'dark'
+export const setDarkModeStorage = (isDark: boolean): void => setStorageItem('theme', isDark ? 'dark' : 'light')
+export const isDarkModeStorage = (): boolean => getStorageItem<string>('theme') === 'dark'
 
-export const setLanguageStorage = (lang: Language): void => localStorage.setItem('preferred-language', lang)
+export const setLanguageStorage = (lang: Language): void => setStorageItem('preferred-language', lang)
+export const getLanguageStorage = (): Language | null => getStorageItem<Language>('preferred-language')
 
-export const getLanguageStorage = (): Language | null => localStorage.getItem('preferred-language') as Language | null
-
-export const setSidebarOpenStorage = (isOpen: boolean): void => localStorage.setItem('sidebar-state', isOpen ? 'open' : 'closed')
-
-export const isSidebarOpenStorage = (): boolean => localStorage.getItem('sidebar-state') === 'open'
+export const setSidebarOpenStorage = (isOpen: boolean): void => setStorageItem('sidebar-state', isOpen ? 'open' : 'closed')
+export const isSidebarOpenStorage = (): boolean => getStorageItem<string>('sidebar-state') === 'open'
